3661e9cc786490b9ad83d345689acbb059a75f98
ko-kamenashi <kamenashi0917@gmail.com>
2020-07-30 16:44:15 +0900

    add time logic to substract several duplicate time

--- src/gCalTotaling.js
+++ src/gCalTotaling.js
@@ -28,6 +28,7 @@ const calendarsId = [
 ];
 
 var today = {
+  // TODO: update read-only
   day: new Date(),
   getDay: function() {
     return this.day
@@ -35,31 +36,40 @@ var today = {
 };
 
   function getTodaySchedules() {
-    let calendars = getCalendars(calendarsId);
-    let events    = getEventsExceptAllDay(calendars)
+    try {
+      let calendars = getCalendars(calendarsId);
+      let events    = getEventsExceptAllDay(calendars)
 
-    writeSpreadSheet(calendars, events);
+      writeSpreadSheet(events);
+    } catch (error) {
+      Logger.log(error);
+    }
   }
 
   function getCalendars(calendarsId) {
     var calendars = [];
+
     calendarsId.map(function(id) {
       calendars.push(CalendarApp.getCalendarById(id));
     });
+
     return calendars;
   }
 
   function getEventsExceptAllDay(calendars) {
     var events = [];
+
     calendars.map(function(cal) {
-      let _today = today.getDay();
+      let _today      = today.getDay();
       let todayEvents = cal.getEventsForDay(_today);
-      todayEvents.map(function(event, i) {
+
+      todayEvents.map(function(event) {
         var bool = exceptAllDayEvents(event);
 
         if (bool) events.push(event);
       });
     });
+
     return events;
   }
 
@@ -67,11 +77,12 @@ var today = {
     let startTime     = event.getStartTime().toTimeString().slice(0, 8);
     let endTime       = event.getEndTime().toTimeString().slice(0, 8);
     let isNotAllEvent = (startTime != '00:00:00' && endTime != '00:00:00') ? true : false;
+
     return isNotAllEvent;
   }
 
-  function writeSpreadSheet(calendars, events) {
-    let file = SpreadsheetApp.getActiveSpreadsheet();
+  function writeSpreadSheet(events) {
+    let file   = SpreadsheetApp.getActiveSpreadsheet();
     let sheet = insertSheetForToday(file);
 
     if (sheet) {
@@ -81,7 +92,7 @@ var today = {
 
   function insertSheetForToday(file) {
     let sheets = file.getSheets();
-    let _today  = today.getDay();
+    let _today = today.getDay();
     let dayStr = _today.getDate().toString();
 
     if (!file.getSheetByName(dayStr)) {
@@ -94,10 +105,46 @@ var today = {
       var calendar  = CalendarApp.getCalendarById(event.getOriginalCalendarId());
       var calName   = calendar != null ? calendar.getName() : 'event@DAC';
       var eventName = event.getTitle() != '' ? event.getTitle() : '予定あり';
+      var startTime = event.getStartTime().toTimeString().slice(0, 8);
+      var endTime   = event.getEndTime().toTimeString().slice(0, 8);
+      var totalTime = getActivityTime(event, events);
 
       sheet.getRange(i + 1, 1).setValue(calName);
       sheet.getRange(i + 1, 2).setValue(eventName);
+      sheet.getRange(i + 1, 3).setValue(startTime);
+      sheet.getRange(i + 1, 4).setValue(endTime);
+      sheet.getRange(i + 1, 5).setValue(totalTime);
     });
+  }
+
+  function getActivityTime(event, events) {
+    var hours        = event.getEndTime().getHours() - event.getStartTime().getHours();
+    var minutes      = event.getEndTime().getMinutes() - event.getStartTime().getMinutes();
+    [hours, minutes] = calcTotalTime(hours, minutes);
+
+    events.map(function(_event) {
+      if (event.getId() != _event.getId() && event.getStartTime() <= _event.getStartTime() && event.getEndTime() >= _event.getEndTime()) {
+        var _hours         = _event.getEndTime().getHours() - _event.getStartTime().getHours();
+        var _minutes       = _event.getEndTime().getMinutes() - _event.getStartTime().getMinutes();
+        [_hours, _minutes] = calcTotalTime(_hours, _minutes);
+
+        hours   -= _hours;
+        minutes -= _minutes;
+
+        [hours, minutes] = calcTotalTime(hours, minutes);
+      }
+    });
+
+    return hours + ':' + minutes + ':00';
+
+    function calcTotalTime(hours, minutes) {
+      if (minutes < 0) {
+        --hours;
+        minutes = 60 + minutes;
+      }
+
+      return [hours, minutes];
+    }
   }
 
   // TODO: make mv column function if there isn't the existing same like function
